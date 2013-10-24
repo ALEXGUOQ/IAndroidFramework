7bd39a8921a7083d63e5a493367fe51c6dc4d20f
storm <zhangqi.dev@qq.com>
2013-10-24 12:32:35 +0800

    Refactor IToast

--- src/com/storm/utils/IToast.java
+++ src/com/storm/utils/IToast.java
@@ -10,11 +10,19 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class IToast {
-	public static void showLong(Context ctx, String text) {
-		Toast.makeText(ctx, text, Toast.LENGTH_LONG).show();
+	public static void showLong(Context context, String text) {
+		Toast.makeText(context, text, Toast.LENGTH_LONG).show();
 	}
 
-	public static void showShort(Context ctx, String text) {
-		Toast.makeText(ctx, text, Toast.LENGTH_SHORT).show();
+	public static void showShort(Context context, String text) {
+		Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+	}
+
+	public static void showLong(Context context, int resId) {
+		Toast.makeText(context, resId, Toast.LENGTH_LONG).show();
+	}
+
+	public static void showShort(Context context, int resId) {
+		Toast.makeText(context, resId, Toast.LENGTH_SHORT).show();
 	}
 }
