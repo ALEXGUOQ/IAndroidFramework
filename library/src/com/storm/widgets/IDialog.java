178e21adcbb87046b9a1c8eee095ecf20b125e81
storm <zhangqi.dev@qq.com>
2013-10-26 21:36:12 +0800

    Add widgets package

---
+++ library/src/com/storm/widgets/IDialog.java
@@ -0,0 +1,15 @@
+/*
+ * @creator Storm
+ * @created_at Oct 26, 2013 5:04:58 PM
+ * Copyright (C) 2013 BOOHEE. All rights reserved.
+ */
+
+package com.storm.widgets;
+
+import android.content.Context;
+
+public class IDialog {
+	public static void show(Context context) {
+		
+	}
+}
