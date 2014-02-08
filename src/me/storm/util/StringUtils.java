51e34279a375404139d6e70b7358b0ea505ac005
storm <zhangqi.dev@qq.com>
2014-02-08 14:18:32 +0800

    Add StringUtils

--- src/me/storm/util/StringUtils.java
+++ src/me/storm/util/StringUtils.java
@@ -1,5 +1,16 @@
 package me.storm.util;
 
 public class StringUtils {
-	
+	public static boolean isBlank(String str) {
+		return (str == null || str.trim().length() == 0);
+	}
+
+	public static boolean isEmpty(String str) {
+		return (str == null || str.length() == 0);
+	}
+
+	public static String htmlEscapeCharsToString(String source) {
+		return isEmpty(source) ? source : source.replaceAll("&lt;", "<").replaceAll("&gt;", ">")
+				.replaceAll("&amp;", "&").replaceAll("&quot;", "\"");
+	}
 }
